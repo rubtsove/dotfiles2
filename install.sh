9303876c6dea68f7d9140b9831f389a7c47c9cd9
Evgeny Rubtsov <evgeny.rubtsov@gmail.com>
2022-01-19 14:45:23 +0500

    some updates

--- install.sh
+++ install.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
 clear 
-set -eo pipefail
+set -e
+
+OS_VER="$(hostnamectl |grep -i "operating"|awk '{print tolower($3)}')"
+if [[ ! -f "$(which git)" ]];then
+  echo -e "\e[0;37mGIT \e[0;31mНЕ\e[0;37m найден\e[0m\n"
+  if [[  "$(id -u)" == 0 ]] && { [[ "${OS_VER}" == "ubuntu" ]] || [[ "${OS_VER}" == "debian" ]] ;} ; then
+    apt -y -qq install git
+  elif [[  "$(id -u)" == 0 ]] && [[ "${OS_VER}" == "centos" ]] ; then
+    yum install -y -q git
+  else
+    echo -e  "запусти сперва скрипт из под \e[0;31mroot\e[0m" && exit
+  fi
+else
+  echo -e "\e[0;37mУра.Пакет \e[0;33mGIT\e[0;37m уже установлен\e[0m"
+fi
 
 ### 
 if [[ ! -d /tmp/dotfiles ]];then 
@@ -16,3 +30,4 @@ for file in ${LIB_PATH} ; do
 done
 
 check_root # function from common.sh
+unset OS_VER
