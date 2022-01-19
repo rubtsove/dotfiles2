fa65b02b1d4dbdcc842f341ffde704bf9e54119b
Evgeny Rubtsov <evgeny.rubtsov@gmail.com>
2022-01-19 11:57:01 +0500

    some updates

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ set -eo pipefail
 
 ### 
 if [[ ! -d /tmp/dotfiles ]];then 
-  echo -e "\e[0;37mКачну репозиторий в: \e[0;33m /tmp/dotfiles/\e[0m"
+  echo -e "\n\e[0;37mКачну репозиторий в: \e[0;33m /tmp/dotfiles/\e[0m"
   git clone --quiet --depth=2 https://github.com/rubtsove/dotfiles2 /tmp/dotfiles &&\
   rm -rf /tmp/dotfiles/.git/
 fi
