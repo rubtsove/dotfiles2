f72f228c389703e41dc849f76c4947aed5a64818
JinGreen <evgeny.rubtsov@gmail.com>
2022-01-19 10:44:06 +0500

    some updates

--- libs/dynmotd_install.sh
+++ libs/dynmotd_install.sh
@@ -14,8 +14,9 @@ if [[ -f "${DYNMOTD_DEST}" ]];then
   echo -e "${GREY}Файл ${ORANGE}/etc/profile.d/dynmotd.sh${GREY} был удален и будет скопирован заново${NOFORMAT}"
 else
   if wget --quiet --spider "${DYNMOTD_SOURCE}"; then
-    echo -e "${GREEN} файл ${ORANGE}dymotd.sh${GREEN} доступен для загрузки${NOFORMAT}"
-    wget --quiet "${DYNMOTD_SOURCE}" --directory-prefix=/etc/profile.d/ 
+    echo -e "${GREEN} файл ${ORANGE}dymotd.sh${GREEN} доступен для загрузки${NOFORMAT}\n"
+    wget --quiet "${DYNMOTD_SOURCE}" --directory-prefix=/etc/profile.d/ &&\
+     chmod +x /etc/profile.d/dynmotd.sh
     echo -e "${GREEN} Конфигурационный файл ${ORANGE}dymotd.sh${GREEN} скопирован в папку: ${ORANGE}/etc/profile.d/${NOFORMAT}"&&\
       log "dymotd.sh успешно скопирован" 
   else
